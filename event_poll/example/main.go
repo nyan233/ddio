4a15e42a8a89e15b0adb4d6c95f0f2556fb91f68
zbh255 <565584326@qq.com>
2022-04-16 21:28:29 +0800

    更新示例文件

--- event_poll/example/main.go
+++ event_poll/example/main.go
@@ -1,52 +1,49 @@
 package main
 
 import (
-	"github.com/zbh255/bilog"
+	"fmt"
 	"github.com/zbh255/nyan/event_poll"
 	"net"
-	"os"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
 )
 
 type SimpleHttpEchoServer struct {
 
 }
 
-func (s *SimpleHttpEchoServer) OnRead(readBuffer, writeBuffer []byte, flags ddio.EventFlags) error {
-	copy(readBuffer,writeBuffer)
+func (s *SimpleHttpEchoServer) OnData(conn ddio.Conn) error {
+	conn.WriteBytes([]byte("HTTP/1.1 200 OK\r\nServer: ddio\r\nContent-Type: text/plain\r\nDate: "))
+	conn.WriteBytes(time.Now().AppendFormat([]byte{}, "Mon, 02 Jan 2006 15:04:05 GMT"))
+	conn.WriteBytes([]byte("\r\nContent-Length: 12\r\n\r\nHello World!"))
 	return nil
 }
 
-func (s *SimpleHttpEchoServer) OnWrite(writeBuffer []byte, flags ddio.EventFlags) error {
-	panic("implement me")
-}
-
 func (s *SimpleHttpEchoServer) OnClose(ev ddio.Event) error {
-	panic("implement me")
+	fmt.Println("connection closed")
+	return nil
 }
 
 func (s *SimpleHttpEchoServer) OnError(ev ddio.Event, err error) {
-	panic("implement me")
+	fmt.Println("connection error: ", err)
 }
 
 func main() {
-	logger := bilog.NewLogger(os.Stdout,bilog.PANIC,bilog.WithTimes(),bilog.WithCaller())
-	var onErr ddio.ErrorHandler = func(err error) {
-		logger.ErrorFromErr(err)
-	}
-
 	config := &ddio.DisPatcherConfig{
-		ConnEvent:      ddio.EVENT_READ,
 		ConnHandler:    &SimpleHttpEchoServer{},
-		ConnErrHandler: onErr,
+		Balanced: &ddio.RoundBalanced{},
 		EngineConfig: &ddio.NetPollConfig{
 			Protocol: 0x1,
-			IP: net.ParseIP("0.0.0.0"),
+			IP: net.ParseIP("192.168.1.150"),
 			Port: 8080,
 		},
 	}
-	engine,err := ddio.NewEngine(ddio.NewTCPListener(ddio.EPOLLIN),onErr,config)
+	engine,err := ddio.NewEngine(ddio.NewTCPListener(ddio.EVENT_LISTENER),config)
 	if err != nil {
-		logger.PanicFromErr(err)
+		panic(err)
 	}
+	_ = http.ListenAndServe("0.0.0.0:9090", nil)
 	engine.Run()
+	_ = engine.Close()
 }
