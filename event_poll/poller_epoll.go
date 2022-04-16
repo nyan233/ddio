d748bb5d1d1da720566c54c0151cf35ad29c9466
zbh255 <565584326@qq.com>
2022-04-16 21:25:21 +0800

    修改poller on epoll中定义错误的事件值

--- event_poll/poller_epoll.go
+++ event_poll/poller_epoll.go
@@ -1,4 +1,5 @@
 //go:build linux
+
 package ddio
 
 import (
@@ -7,79 +8,75 @@ import (
 	"time"
 )
 
+const (
+	eV_READ     = unix.EPOLLET | unix.EPOLLIN
+	eV_WRITE    = unix.EPOLLET | unix.EPOLLOUT
+	eV_LISTENER = unix.EPOLLIN
+	eV_CLOSE    = unix.EPOLLHUP
+)
+
 type poller struct {
 	*epoll
-	onErr func(err error)
-	events map[int]Event
-	mu *sync.Mutex
+	// Raw Event
+	// Fd : Raw Event
+	events map[int]EventFlags
+	mu     *sync.Mutex
 }
 
-func NewPoller(onErr func(err error)) *poller {
-	ep,err := NewEpoll()
+func NewPoller() (*poller,error) {
+	ep, err := NewEpoll()
 	if err != nil {
-		onErr(err)
-		return nil
+		return nil,err
 	}
 	return &poller{
 		ep,
-		onErr,
-		make(map[int]Event,256),
+		make(map[int]EventFlags, 256),
 		&sync.Mutex{},
-	}
+	},nil
 }
 
-func (p poller) Exec(maxEvent int,timeOut time.Duration) ([]Event,error) {
-	events := make([]unix.EpollEvent,maxEvent)
-	nEvent, err := p.Wait(events,timeOut)
+func (p poller) Exec(maxEvent int, timeOut time.Duration) ([]Event, error) {
+	events := make([]unix.EpollEvent, maxEvent)
+	nEvent, err := p.Wait(events, timeOut)
 	if err != nil {
 		return nil, err
 	}
 	events = events[:nEvent]
-	stdEvents := make([]Event,nEvent)
+	stdEvents := make([]Event, nEvent)
 	for i := 0; i < nEvent; i++ {
 		event := events[i]
+		p.mu.Lock()
 		stdEvents[i] = Event{
 			sysFd: event.Fd,
-			event: func(flags uint32) EventFlags {
-				var evFlags EventFlags
-				if flags & unix.EPOLLET == unix.EPOLLET {
-					evFlags |= 0
-				} else if flags & unix.EPOLLIN == unix.EPOLLIN {
-					evFlags |= EVENT_READ
-				} else if flags & unix.EPOLLOUT == unix.EPOLLOUT {
-					evFlags |= EVENT_WRITE
-				} else if flags & unix.EPOLLHUP == unix.EPOLLHUP {
-					evFlags |= EVENT_CLOSE
-				}
-				return evFlags
-			}(event.Events),
+			event: epollToEvent(int(p.events[int(event.Fd)])),
 		}
+		p.mu.Unlock()
 	}
-	return stdEvents,nil
+	return stdEvents, nil
 }
 
 func (p poller) With(event *Event) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	flags := 0
-	if event.event & EVENT_READ == EVENT_READ {
-		flags |= unix.EPOLLET | unix.EPOLLIN
-	} else if event.event & EVENT_WRITE == EVENT_WRITE {
-		flags |= unix.EPOLLET | unix.EPOLLOUT
-	} else if event.event & EVENT_CLOSE == EVENT_CLOSE {
-		flags |= unix.EPOLLHUP
-	}
-	vEvent := *event
+	flags := eventToEpoll(event.event)
 	event.event = EventFlags(flags)
-	err :=p.AddEvent(event)
+	err := p.AddEvent(event)
 	if err == nil {
-		p.events[int(event.fd())] = vEvent
+		p.events[int(event.fd())] = event.Flags()
 	}
 	return err
 }
 
 func (p poller) Modify(event *Event) error {
-	return p.ModEvent(event)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	flags := eventToEpoll(event.Flags())
+	event.event = EventFlags(flags)
+	err := p.ModEvent(event)
+	if err == nil {
+		p.events[int(event.fd())] = event.Flags()
+	}
+	return err
 }
 
 func (p poller) Cancel(event *Event) error {
@@ -95,13 +92,45 @@ func (p poller) Cancel(event *Event) error {
 func (p poller) AllEvents() []Event {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	events := make([]Event,0,len(p.events))
-	for _,v := range p.events {
-		events = append(events,v)
+	events := make([]Event, 0, len(p.events))
+	for k, v := range p.events {
+		events = append(events, Event{
+			sysFd: int32(k),
+			event: epollToEvent(int(v)),
+		})
 	}
 	return events
 }
 
 func (p poller) Exit() error {
 	return nil
+}
+
+// Utils
+func eventToEpoll(flags EventFlags) int {
+	var epFlags EventFlags
+	if flags&EVENT_READ == EVENT_READ {
+		epFlags |= eV_READ
+	} else if flags&EVENT_WRITE == EVENT_WRITE {
+		epFlags |= eV_WRITE
+	} else if flags&EVENT_CLOSE == EVENT_CLOSE {
+		epFlags |= eV_CLOSE
+	} else if flags&EVENT_LISTENER == EVENT_LISTENER {
+		epFlags |= eV_LISTENER
+	}
+	return int(epFlags)
+}
+
+func epollToEvent(epollEvent int) EventFlags {
+	var flags EventFlags
+	if epollEvent&eV_READ == eV_READ {
+		flags |= EVENT_READ
+	} else if epollEvent&eV_WRITE == eV_WRITE {
+		flags |= EVENT_WRITE
+	} else if epollEvent&eV_LISTENER == eV_LISTENER {
+		flags |= EVENT_LISTENER
+	} else if epollEvent&eV_CLOSE == eV_CLOSE {
+		flags |= EVENT_CLOSE
+	}
+	return flags
 }
