47f9d6db609dacad6cd7ee8dc348c9004caaf156
zbh255 <565584326@qq.com>
2022-04-16 21:23:24 +0800

    添加抽象多平台的IO接口

---
+++ event_poll/internal/conn_handler/before_conn_unix.go
@@ -0,0 +1,23 @@
+//go:build linux || darwin || freebsd
+package conn_handler
+
+import (
+	"golang.org/x/sys/unix"
+	"net"
+)
+
+type BeforeConnHandler struct {
+
+}
+
+func (b *BeforeConnHandler) NioRead(fd int,buf []byte) (int, error) {
+	return unix.Read(fd,buf)
+}
+
+func (b *BeforeConnHandler) NioWrite(fd int,buf []byte) (int, error) {
+	return unix.Write(fd,buf)
+}
+
+func (b *BeforeConnHandler) Addr(fd int) net.Addr {
+	return nil
+}
