d3d5b1c6164b78e8e1da632655a1f809f3c3a2cf
zbh255 <565584326@qq.com>
2022-04-18 23:15:36 +0800

    - 添加最大的主从Reactor数量 - 修改EventLoop Interface注册的参数类型，因为内部会修改Event的一些数据，所以对于上层代码来说可能会有迷惑的行为

--- event_poll/interface.go
+++ event_poll/interface.go
@@ -7,8 +7,13 @@ import (
 
 // ConnectionEventHandler 连接事件处理器
 type ConnectionEventHandler interface {
-	// OnData 接收到数据时触发
-	OnData(conn Conn) error
+	// OnInit 初始化连接的一些参数
+	// 注意: 该方式是在读事件就绪时被触发
+	OnInit() ConnConfig
+
+	// OnData 接收到完整数据时触发
+	// 接收的数据量可以被设置
+	OnData(conn *TCPConn) error
 
 	// OnClose 对端关闭事件触发
 	OnClose(ev Event) error
@@ -38,14 +43,14 @@ type EventLoop interface {
 	Exit() error
 
 	// With 往轮询器中添加事件
-	With(event *Event) error
+	With(event Event) error
 
 	// Modify 修改轮询器中事件的属性
 	// 一般用于Epoll OnceShot事件
-	Modify(event *Event) error
+	Modify(event Event) error
 
 	// Cancel 取消事件的监听
-	Cancel(event *Event) error
+	Cancel(event Event) error
 
 	// AllEvents 获取所有监听的事件
 	AllEvents() []Event
@@ -55,13 +60,15 @@ type EventLoop interface {
 type Conn interface {
 	// TakeReadBytes 拿出所有读取的字节
 	TakeReadBytes() []byte
-	// TakeWriteBuffer 拿出Conn绑定的写入缓冲
-	// 该接口针对较大的数据使用
-	TakeWriteBuffer() *[]byte
-	// GrowWriteBuffer 扩容缓冲区
-	GrowWriteBuffer(buf *[]byte, nCap int) bool
 	// WriteBytes 该接口针对小数据量
 	WriteBytes(p []byte)
+	// RegisterAfterHandler 注册处理后续读数据的处理器
+	// TODO 是否可用于将之后的数据交给ZeroCopy系列函数来处理
+	RegisterAfterHandler(hd ConnAfterCenterHandler)
+	// Next 设置了OnDataNBlock时读不到完整的数据时
+	// 可以调用该方法接着读取N * Block的内容
+	// 调用该方法会使OnData事件重新触发
+	Next(nBlock int)
 	// Close 关闭连接
 	// 非立即关闭，采取延迟关闭的策略
 	Close() error
--- event_poll/stdVar.go
+++ event_poll/stdVar.go
@@ -8,19 +8,28 @@ import "errors"
 type EventFlags int
 
 const (
-	EVENT_READ     EventFlags = 0x01                     // 监听可读事件
-	EVENT_WRITE    EventFlags = 0x10                     // 监听可写事件
-	EVENT_CLOSE    EventFlags = 0x100                    // 监听连接关闭事件
-	EVENT_LISTENER EventFlags = 0x04                     // 监听连接建立事件
+	EVENT_READ     EventFlags = 0x01   // 监听可读事件
+	EVENT_WRITE    EventFlags = 0x10   // 监听可写事件
+	EVENT_CLOSE    EventFlags = 0x100  // 监听连接关闭事件
+	EVENT_LISTENER EventFlags = 0x04   // 监听连接建立事件
+	EVENT_ERROR    EventFlags = 0x1000 // 监听错误事件
 )
 
+const (
+	// MAX_MASTER_LOOP_SIZE 负责监听接收新连接的主Reactor的goroutine最大数量
+	MAX_MASTER_LOOP_SIZE = 32
+	// MAX_SLAVE_LOOP_SIZE 主Reactor绑定的负责处理连接事件的从Reactor的goroutine最大数量
+	MAX_SLAVE_LOOP_SIZE = 64
+)
 
 // 一些错误
 var (
 	ErrorEpollClosed = errors.New("epoll is closed")
-	ErrRead = errors.New("read error: ")
-	ErrWrite = errors.New("write error: ")
+	ErrRead          = errors.New("read error: ")
+	ErrWrite         = errors.New("write error: ")
 )
 
 // NewBalance 派生负载均衡器的工厂方法
 type NewBalance func() Balanced
+
+type ConnAfterCenterHandler func(fd int) error
