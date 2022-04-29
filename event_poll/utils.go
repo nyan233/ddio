578203354b726efdcc6656549485a6c4da1d8544
zbh255 <565584326@qq.com>
2022-04-29 22:18:27 +0800

    utils新增对Ip地址进行Parse的代码及测试

--- event_poll/utils.go
+++ event_poll/utils.go
@@ -1,8 +1,46 @@
 package ddio
 
-import "unsafe"
+import (
+	"errors"
+	"net"
+	"strconv"
+	"strings"
+	"unsafe"
+)
 
 func noescape(pointer unsafe.Pointer) unsafe.Pointer {
 	x := uintptr(pointer)
 	return unsafe.Pointer(x ^ 0)
+}
+
+func parseAddress(addr string) (config NetPollConfig, argMap map[string]string, err error) {
+	argSlice := strings.Split(strings.SplitN(addr, "?", 2)[1], "&")
+	argMap = make(map[string]string, len(argSlice)/2)
+	for _, v := range argSlice {
+		kAndV := strings.Split(v, "=")
+		argMap[kAndV[0]] = kAndV[1]
+	}
+	connProtocol := strings.Split(strings.SplitN(addr, "?", 2)[0], "//")
+	switch {
+	case strings.EqualFold(connProtocol[0], "tcp:"):
+		ipSplit := strings.Split(connProtocol[1], ":")
+		switch {
+		case len(ipSplit) == 2:
+			config.Protocol = TCP_V4
+		case len(ipSplit) > 2:
+			config.Protocol = TCP_V6
+		default:
+			err = errors.New("ip format not supported")
+			return
+		}
+		// IPV6地址有简便表示多个零的写法，要对这种方法做特殊处理
+		// 比如这个地址:fe80::1029:f994:b74a:7bef,::处缺少了3组零位
+		// net.ParseIP中排除Port
+		ip := net.ParseIP(connProtocol[1][:len(connProtocol[1]) - len(ipSplit[len(ipSplit) - 1]) - 1])
+		config.IP = ip
+		config.Port, err = strconv.Atoi(ipSplit[len(ipSplit)-1])
+	default:
+		return config, nil, errors.New("not supported protocol")
+	}
+	return
 }
