316184a406f7fa9401f95e0d2804c6ae5ee44dbe
zbh255 <565584326@qq.com>
2022-05-01 01:45:38 +0800

    修复Poller-Epoll wait传入的超时参数问题

--- event_poll/poller_epoll.go
+++ event_poll/poller_epoll.go
@@ -45,7 +45,7 @@ func NewPoller() (*poller, error) {
 
 func (p poller) Exec(receiver []Event, timeOut time.Duration) (int, error) {
 	events := p.pool.Get().([]unix.EpollEvent)[:cap(receiver)]
-	nEvent, err := p.Wait(events, timeOut.Milliseconds())
+	nEvent, err := p.Wait(events, timeOut)
 	if err != nil {
 		return 0, err
 	}
